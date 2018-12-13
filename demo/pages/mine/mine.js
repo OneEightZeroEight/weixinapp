f87306aa5e4a8d36e6e7d7bd63637c916d85201c
wscats <2587575267@qq.com>
2018-12-13 11:06:40 +0800

    使用ui

--- demo/pages/mine/mine.js
+++ demo/pages/mine/mine.js
@@ -1,21 +1,95 @@
-// camera.js
 Page({
-  data:{
-    src:""
-  },
-  takePhoto() {
-    const ctx = wx.createCameraContext()
-    ctx.takePhoto({
-      quality: 'high',
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          src: res.tempImagePath
-        })
+  data: {
+    showLeft1: false,
+    showLeft2: false,
+    showRight1: false,
+    showRigh2: false,
+    current: 'homepage',
+    visible2: false,
+    //小程序没有refs，所以只能用动态布尔值控制关闭
+    toggle: false,
+    toggle2: false,
+    actions2: [
+      {
+        name: '删除',
+        color: '#ed3f14'
       }
-    })
+    ],
+    actions: [
+      {
+        name: '喜欢',
+        color: '#fff',
+        fontsize: '20',
+        width: 100,
+        icon: 'like',
+        background: '#ed3f14'
+      },
+      {
+        name: '返回',
+        width: 100,
+        color: '#80848f',
+        fontsize: '20',
+        icon: 'undo'
+      }
+    ]
   },
-  error(e) {
-    console.log(e.detail)
+  toggleLeft1() {
+    this.setData({
+      showLeft1: !this.data.showLeft1
+    });
+  },
+  toggleLeft2() {
+    this.setData({
+      showLeft2: !this.data.showLeft2
+    });
+  },
+  toggleRight1() {
+    this.setData({
+      showRight1: !this.data.showRight1
+    });
+  },
+  toggleRight2() {
+    this.setData({
+      showRight2: !this.data.showRight2
+    });
+  },
+  handleChange({ detail }) {
+    this.setData({
+      current: detail.key
+    });
+  }, handleCancel2() {
+    this.setData({
+      visible2: false,
+      toggle: this.data.toggle ? false : true
+    });
+    console.log(this.data.toggle, 111111111)
+  },
+  handleClickItem2() {
+    const action = [...this.data.actions2];
+    action[0].loading = true;
+
+    this.setData({
+      actions2: action
+    });
+
+    setTimeout(() => {
+      action[0].loading = false;
+      this.setData({
+        visible2: false,
+        actions2: action,
+        toggle: this.data.toggle ? false : true
+      });
+
+    }, 2000);
+  },
+  handlerCloseButton() {
+    this.setData({
+      toggle2: this.data.toggle2 ? false : true
+    });
+  },
+  actionsTap() {
+    this.setData({
+      visible2: true
+    });
   }
-})
+});
